b339d1ee5fc33363d804096b38df2a4db8021ea1
Ostricker <hrazdil.jan@gmail.com>
2019-01-18 14:07:31 +0100

    Tvorba GUI

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Alienware Game Arena CasparCG Client by Jan Olšanský");
+        primaryStage.setScene(new Scene(root, 1270,750));
         primaryStage.show();
     }
 
